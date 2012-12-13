50d823c5643a948da63ff8bedabdb18f56da1595
Siebert Lubbe <siebert.lubbe@hubonline.com.au>
2012-12-13 23:13:03 +1100

    Fixing typo

--- lib/populate_distances.rb
+++ lib/populate_distances.rb
@@ -6,7 +6,7 @@ suburbs.each do |from_suburb|
   suburbs.each do |to_suburb|
     distance = GeoDistance.distance(from_suburb.latitude, from_suburb.longitude, to_suburb.latitude, to_suburb.longitude)
     Distance.create(:from_suburb => from_suburb.name, :from_state => from_suburb.state,
-                          :to_suburb => to_suburb.name, :to_suburb => to_suburb.state, 
+                          :to_suburb => to_suburb.name, :to_state => to_suburb.state,
                           :distance => distance)
   end
 end
